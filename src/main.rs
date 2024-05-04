a70246aec5232b1c35b436d40aabbb2b74144034
Niklas Cathor <niklas.cathor@gmx.de>
2024-05-04 15:41:12 +0200

    Simple shell, via USB-serial

--- src/main.rs
+++ src/main.rs
@@ -4,49 +4,132 @@
 
 #![no_std]
 #![no_main]
+#![feature(async_fn_traits, async_closure)]
 
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_futures::join::join;
 use embassy_rp::bind_interrupts;
-use embassy_rp::peripherals::PIO0;
-use embassy_rp::pio::{InterruptHandler, Pio};
-use embassy_time::{Duration, Timer};
+use embassy_rp::peripherals::{PIO0, USB};
+use embassy_rp::{pio, usb};
+use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
+use embassy_sync::mutex::Mutex;
+use embassy_time::Timer;
+use embassy_usb::class::cdc_acm;
 use {defmt_rtt as _, panic_probe as _};
 
 mod leds;
+mod shell;
 
 bind_interrupts!(struct Irqs {
-    PIO0_IRQ_0 => InterruptHandler<PIO0>;
+    PIO0_IRQ_0 => pio::InterruptHandler<PIO0>;
+    USBCTRL_IRQ => usb::InterruptHandler<USB>;
 });
 
 /// Number of LEDs on the board. This will vary in the future, depending on hardware revision.
 const NUM_LEDS: usize = 111;
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = embassy_rp::init(Default::default());
-    let Pio {
-        mut common, sm0, ..
-    } = Pio::new(p.PIO0, Irqs);
-    let ws2812: leds::Ws2812<'_, _, 0, NUM_LEDS> =
-        leds::Ws2812::new(&mut common, sm0, p.DMA_CH0, p.PIN_25);
-    let mut leds = leds::Leds::new(ws2812);
+static LEDS: Mutex<ThreadModeRawMutex, Option<leds::Leds<'static, PIO0, 0, NUM_LEDS>>> =
+    Mutex::new(None);
 
-    leds.startup_colors().await;
-    Timer::after_millis(2).await;
-    leds.set_rgb8(110, (32, 0, 0));
-    leds.flush().await;
-
-    loop {
-        info!("Loop...");
-        Timer::after_secs(3).await;
-        leds.rainbow_bounce(Duration::from_millis(40)).await;
-        // ws2812.rainbow_bounce().await;
-        // info!("Bounce done, setting off");
-        // Timer::after_millis(1).await;
-        // ws2812.off().await;
-        // info!("Waiting...");
-        // Timer::after_secs(3).await;
-        // info!("Here we go again!");
+#[embassy_executor::task]
+async fn startup_leds() {
+    {
+        if let Some(leds) = LEDS.lock().await.as_mut() {
+            leds.startup_colors().await;
+            Timer::after_millis(2).await;
+            leds.set_rgb8(110, (32, 0, 0));
+            leds.flush().await;
+        }
     }
+    loop {
+        Timer::after_secs(2).await;
+    }
+}
+
+#[embassy_executor::main]
+async fn main(spawner: Spawner) {
+    let p = embassy_rp::init(Default::default());
+    let pio::Pio {
+        mut common, sm0, ..
+    } = pio::Pio::new(p.PIO0, Irqs);
+    {
+        *(LEDS.lock().await) = Some(leds::Leds::new(leds::Ws2812::new(
+            &mut common,
+            sm0,
+            p.DMA_CH0,
+            p.PIN_25,
+        )));
+    }
+
+    let usb_driver = usb::Driver::new(p.USB, Irqs);
+    let mut config = embassy_usb::Config::new(0x1D50, 0xACAB);
+    config.manufacturer = Some("Architeuthis Flux");
+    config.product = Some("Jumperless");
+    config.serial_number = Some("0");
+
+    info!("2");
+
+    config.device_class = 0xEF;
+    config.device_sub_class = 0x02;
+    config.device_protocol = 0x01;
+    config.composite_with_iads = true;
+
+    // Create embassy-usb DeviceBuilder using the driver and config.
+    // It needs some buffers for building the descriptors.
+    let mut config_descriptor = [0; 256];
+    let mut bos_descriptor = [0; 256];
+    let mut control_buf = [0; 64];
+
+    let mut state = cdc_acm::State::new();
+
+    let mut builder = embassy_usb::Builder::new(
+        usb_driver,
+        config,
+        &mut config_descriptor,
+        &mut bos_descriptor,
+        &mut [], // no msos descriptors
+        &mut control_buf,
+    );
+
+    let mut class = cdc_acm::CdcAcmClass::new(&mut builder, &mut state, 64);
+
+    let mut usb = builder.build();
+
+    let usb_future = usb.run();
+
+    spawner.spawn(startup_leds()).unwrap();
+    // let led_task = async {
+    //     leds.startup_colors().await;
+    //     Timer::after_millis(2).await;
+    //     leds.set_rgb8(110, (32, 0, 0));
+    //     leds.flush().await;
+    //     loop {
+    //         Timer::after_secs(10).await;
+    //     }
+    // };
+
+    let shell_future = async {
+        loop {
+            class.wait_connection().await;
+            info!("USB Serial Connected");
+            let mut shell: shell::Shell<'_, '_, 256> = shell::Shell::new(&mut class);
+            let _ = shell.run().await;
+            info!("USB Serial Disconnected");
+        }
+    };
+    join(usb_future, shell_future).await;
+
+    // loop {
+    //     info!("Loop...");
+    //     Timer::after_secs(3).await;
+    //     leds.rainbow_bounce(Duration::from_millis(40)).await;
+    //     // ws2812.rainbow_bounce().await;
+    //     // info!("Bounce done, setting off");
+    //     // Timer::after_millis(1).await;
+    //     // ws2812.off().await;
+    //     // info!("Waiting...");
+    //     // Timer::after_secs(3).await;
+    //     // info!("Here we go again!");
+    // }
 }
---
+++ src/shell.rs
@@ -0,0 +1,114 @@
+use embassy_rp::{peripherals::USB, usb::Driver};
+use embassy_time::Duration;
+use embassy_usb::{class::cdc_acm::CdcAcmClass, driver::EndpointError};
+
+pub struct Disconnected {}
+
+impl From<EndpointError> for Disconnected {
+    fn from(val: EndpointError) -> Self {
+        match val {
+            EndpointError::BufferOverflow => defmt::panic!("Buffer overflow"),
+            EndpointError::Disabled => Disconnected {},
+        }
+    }
+}
+
+pub struct Overflow;
+
+impl From<Overflow> for &'static [u8] {
+    fn from(_: Overflow) -> Self {
+        "Input buffer overflow".as_bytes()
+    }
+}
+
+pub struct Shell<'a, 'b, const BUF_SIZE: usize> {
+    class: &'a mut CdcAcmClass<'b, Driver<'b, USB>>,
+    input_buf: [u8; BUF_SIZE],
+    cursor: usize,
+}
+
+const HELP: &[&[u8]] = &[
+    b"Available instructions:\r\n",
+    b"  help            Print this help text\r\n",
+    b"  rainbow-bounce  Play rainbow animation\r\n",
+];
+
+
+impl<'a, 'b, const BUF_SIZE: usize> Shell<'a, 'b, BUF_SIZE> {
+    pub fn new(class: &'a mut CdcAcmClass<'b, Driver<'b, USB>>) -> Self {
+        Self {
+            class,
+            input_buf: [0; BUF_SIZE],
+            cursor: 0,
+        }
+    }
+
+    pub async fn run(&mut self) -> Result<(), Disconnected> {
+        let mut buf = [0; 64];
+        loop {
+            let n = self.class.read_packet(&mut buf).await?;
+
+            let mut submit = false;
+
+            for &c in &buf[..n] {
+                if c == b'\r' {
+                    submit = true;
+                } else if c == 0x08 {
+                    // backspace
+                    self.cursor -= 1;
+                    self.input_buf[self.cursor] = 0;
+                } else if c.is_ascii_graphic() || c.is_ascii_whitespace() {
+                    self.input_buf[self.cursor] = c;
+                    self.cursor += 1;
+                }
+            }
+
+            self.prompt().await?;
+
+            if submit {
+                self.class.write_packet(b"\r\n").await?;
+                self.process().await?;
+            }
+        }
+    }
+
+    async fn prompt(&mut self) -> Result<(), Disconnected> {
+        self.class.write_packet(b"\r> ").await?;
+        if self.cursor > 0 {
+            self.class
+                .write_packet(&self.input_buf[0..self.cursor])
+                .await?;
+        }
+        Ok(())
+    }
+
+    async fn process(&mut self) -> Result<(), Disconnected> {
+        if self.cursor == 0 {
+            self.prompt().await?;
+            return Ok(());
+        }
+        if let Ok(input) = core::str::from_utf8(&self.input_buf[0..self.cursor]) {
+            match input.trim() {
+                "help" => {
+                    for line in HELP {
+                        self.class.write_packet(line).await?;
+                    }
+                }
+                "rainbow-bounce" => {
+                    if let Some(leds) = crate::LEDS.lock().await.as_mut() {
+                        leds.rainbow_bounce(Duration::from_millis(40)).await;
+                    }
+                }
+                _ => {
+                    self.class
+                        .write_packet(b"Error: Unknown instruction\r\n")
+                        .await?
+                }
+            }
+        }
+        self.input_buf.fill(0);
+        self.cursor = 0;
+        self.prompt().await?;
+        Ok(())
+    }
+}
